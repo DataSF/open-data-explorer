117758401ca609315870cc83a87323b991ea857a
Jason Lally <jason.lally@gmail.com>
2017-03-01 15:27:44 -0800

    modify subtitle component to handle multiple filter options

--- app/components/ChartExperimental/ChartExperimentalSubTitle.jsx
+++ app/components/ChartExperimental/ChartExperimentalSubTitle.jsx
@@ -4,14 +4,24 @@ import titleize from 'titleize'
 import moment from 'moment'
 
 class ChartExperimentalSubTitle extends Component {
+  constructor (props) {
+    super(props)
+
+    this.filterCategories = this.filterCategories.bind(this)
+    this.filterDates = this.filterDates.bind(this)
+    this.filterNumbers = this.filterNumbers.bind(this)
+  }
+
   filterCategories (columnFilter, columnFilterName) {
     let subtitle = ''
     let fitlerCategory
+    let { columns } = this.props
+
     if (typeof columnFilter.options['selected'] === 'string') {
       subtitle += titleize(columnFilter.options['selected'].toLowerCase())
     } else {
       subtitle = columnFilter.options['selected'].map(function (item) {
-        return titleize(item.toLowerCase())
+        return columnFilter.options.filterType === 'category' ? titleize(item) : titleize(columns[item].name)
       }).join(', ')
     }
     if (subtitle.length > 0) {
@@ -19,7 +29,7 @@ class ChartExperimentalSubTitle extends Component {
       subtitle = 'Filtering by ' + titleize(columnFilterName)
       subtitle = subtitle + ': ' + fitlerCategory
     }
-    return <span key='filterCatText'>{subtitle}</span>
+    return <div key={columnFilterName}>{subtitle}</div>
   }
 
   filterDates (columnFilter, columnFilterName) {
@@ -30,7 +40,7 @@ class ChartExperimentalSubTitle extends Component {
     subtitle = 'Filtering by ' + titleize(columnFilterName)
     fitlerCategory = 'Only Showing Records Between ' + minDt + ' and ' + maxDt
     subtitle = subtitle + ': ' + fitlerCategory
-    return <span key='filterDateText'>{subtitle}</span>
+    return <div key={columnFilterName}>{subtitle}</div>
   }
 
   filterNumbers (columnFilter, columnFilterName) {
@@ -42,30 +52,51 @@ class ChartExperimentalSubTitle extends Component {
     subtitle = 'Filtering by ' + titleize(columnFilterName)
     fitlerCategory = 'Only Showing Records with Values Between ' + start + ' and ' + end
     subtitle = subtitle + ': ' + fitlerCategory
-    return <span key='filterNumText'>{subtitle}</span>
+    return <div key={columnFilterName}>{subtitle}</div>
   }
 
   buildSubTitle (filters, columns) {
+    const builders = {
+      'category': this.filterCategories,
+      'booleanCategory': this.filterCategories,
+      'numericRange': this.filterNumbers,
+      'dateRange': this.filterDates
+    }
+
     let subtitle = []
+    let newArray
     if (!_.isEmpty(filters)) {
       let filterKeys = Object.keys(filters)
+      newArray = filterKeys.map(function (key) {
+        let filter = filters[key]
+        let column = columns[key] || null
+        let columnName = column !== null ? column.name : 'Boolean Fields'
+        if (!_.isEmpty(filter)) {
+          return builders[filter.options.filterType](filter, columnName)
+        }
+        return null
+      })
+      /*
       for (let i = 0; i < filterKeys.length; i++) {
         let filter = filters[filterKeys[i]]
-        let column = columns[filterKeys[i]]
+        let column = columns[filterKeys[i]] || null
         if (!_.isEmpty(filter)) {
           if (filter.options.filterType === 'category' && filter.options.selected !== null) {
-            subtitle = subtitle.concat([this.filterCategories(filter, column.name), <br key='filterCatBR' />])
+            subtitle = subtitle.concat([this.filterCategories(filter, column.name), <br />])
+          } else if (filter.options.filterType === 'booleanCategory') {
+            subtitle = subtitle.concat([this.filterCategories(filter, 'Boolean Fields'), <br key='filterBoolBR' />])
           } else if (filter.options.filterType === 'dateRange') {
             subtitle = subtitle.concat([this.filterDates(filter, column.name), <br key='filterDateBR' />])
           } else if (filter.options.filterType === 'numericRange') {
             subtitle = subtitle.concat([this.filterNumbers(filters[filterKeys[i]], column.name), <br key='filterNumBR' />])
           }
         }
-      }
+      } */
     }
-    return subtitle
+    return newArray
   }
   render () {
+    console.log(this.props)
     let {filters, columns} = this.props
     let subtitleStuff = this.buildSubTitle(filters, columns)
     return (
