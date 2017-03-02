cfd9ca528057c31a5c14d6fe7290e6cfb9f58e12
j9recurses <janine.heiser@gmail.com>
2017-03-02 15:12:14 -0800

    various fixes for grpby and reworks formatting on the chart data

--- app/helpers/sumObj.js
+++ app/helpers/sumObj.js
@@ -2,9 +2,8 @@ module.exports = function sumObj (obj, skipKey) {
   let sum = 0
   for (let el in obj) {
     if (el !== skipKey) {
-      console.log(el)
       if (obj.hasOwnProperty(el)) {
-        sum += parseFloat(obj[el])
+        sum += obj[el]
       }
     }
   }
