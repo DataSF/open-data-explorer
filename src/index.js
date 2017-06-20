5ea14e5e1cacae7be16666826aa0b9d9e10e5689
Jason Lally <jason.lally@gmail.com>
2017-06-20 13:49:36 -0700

    switch to browser history

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles/bootstrap-explorer.css'
 import React from 'react'
 import { render } from 'react-dom'
-import { hashHistory } from 'react-router'
+import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import Root from './containers/Root'
 import configureStore from './store/configureStore'
@@ -26,7 +26,7 @@ const initialState = {
   }
 }
 const store = configureStore(initialState)
-const history = syncHistoryWithStore(hashHistory, store)
+const history = syncHistoryWithStore(browserHistory, store)
 render(
   <Root store={store} history={history} />,
   document.getElementById('root')
