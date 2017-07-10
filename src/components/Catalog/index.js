75c01a7a759da74d1cab779da67a6883950acd72
Jason Lally <jason.lally@gmail.com>
2017-07-10 12:23:36 -0700

    change layout of catalog

--- src/components/Catalog/index.js
+++ src/components/Catalog/index.js
@@ -1,39 +1,58 @@
 import './@Catalog.css'
 
 import React from 'react'
-import { Hits, SearchBox, RefinementList, CurrentRefinements, ClearAll, Pagination, Panel, Stats } from 'react-instantsearch/dom'
+import { Hits, SearchBox, RefinementList, CurrentRefinements, ClearAll, Pagination, Panel, Highlight } from 'react-instantsearch/dom'
+import { connectStats } from 'react-instantsearch/connectors'
 import { Grid, Row, Col, Panel as BSPanel } from 'react-bootstrap'
 import orderBy from 'lodash/orderBy'
 import { Link } from 'react-router'
 import slugify from 'underscore.string/slugify'
 import moment from 'moment'
 
+const CustomStats = ({nbHits}) => <span className={'ais-Stats__root clearfix'}>{nbHits} results found.</span>
+
+const ConnectedStats = connectStats(CustomStats)
+
 const Record = (clearSearch, {hit}) => (
-  <BSPanel header={<Link to={`${'/' + slugify(hit.category) + '/' + slugify(hit.name) + '/' + hit.systemID}`} onClick={clearSearch}>{hit.name}</Link>} className='Catalog__record' bsStyle='primary'>
-    <div className={'Catalog__record-body-wrapper'}>
-      <div className={'Catalog__record-meta clearfix'}>
-        <div className={'Catalog__record-meta-title'}>Data updated</div>
-        <div className={'Catalog__record-meta-value'}>{moment(hit.rowsUpdatedAt * 1000).fromNow()}</div>
-        <div className={'Catalog__record-meta-title'}>Target update schedule</div>
-        <div className={'Catalog__record-meta-value'}>{hit.publishingFrequency}</div>
-        <div className={'Catalog__record-meta-title'}>Category</div>
-        <div className={'Catalog__record-meta-value'}>{hit.category}</div>
-      </div>
-      <div className={'Catalog__record-description clearfix'}>
-        <p>{hit.description}</p>
-        {hit.tags ? (
-          <p>Tags {hit.tags.join(', ')}</p>
-          ) : false 
-        }
-      </div>
+  <BSPanel 
+    header={<Link to={`${'/' + slugify(hit.category) + '/' + slugify(hit.name) + '/' + hit.systemID}`} onClick={clearSearch}>
+        <Highlight attributeName='name' hit={hit} tagName='mark'/>
+      </Link>} 
+    className='Catalog__record' 
+    bsStyle='primary'>
+    <div className={'Catalog__record-meta clearfix'}>
+      <div className={'Catalog__record-meta-title'}>Data updated</div>
+      <div className={'Catalog__record-meta-value'}>{moment(hit.rowsUpdatedAt * 1000).fromNow()}</div>
+      <div className={'Catalog__record-meta-title'}>Target update schedule</div>
+      <div className={'Catalog__record-meta-value'}>{hit.publishingFrequency}</div>
+      <div className={'Catalog__record-meta-title'}>Category</div>
+      <div className={'Catalog__record-meta-value'}><Highlight attributeName='category' hit={hit} tagName='mark'/></div>
+    </div>
+    <div className={'Catalog__record-description clearfix'}>
+      <p className={'Catalog__record-description-text'}>
+        <Highlight attributeName='description' hit={hit} tagName='mark'/>
+      </p>
+      {hit.tags ? (
+        <p className={'Catalog__record-tags'}>
+          <strong>Tags</strong> <span className={'App--font-thin'}>{hit.tags.join(', ')}</span></p>
+        ) : false 
+      }
     </div>
   </BSPanel>
 )
 
 const Search = ({clearSearch}) => (
-  <Grid fluid className={'Catalog'}>
+  <Grid className={'Catalog'}>
     <Row className='Catalog__search'>
-      <Col sm={3} className='Catalog__refine'>
+      <Col sm={12}>
+        <SearchBox autoFocus />
+      </Col>
+      <Col sm={12} className={'Catalog__currentRefinements'}>
+        <ConnectedStats />
+          <CurrentRefinements className={'clearfix'} />
+          <ClearAll />
+      </Col>
+      <Col sm={2} className='Catalog__refine'>
         <Panel title='Categories'>
           <RefinementList className='Catalog__refine--category' attributeName='category' />
         </Panel>
@@ -41,15 +60,7 @@ const Search = ({clearSearch}) => (
           <RefinementList className='Catalog__refine--department' attributeName='publishing_dept' withSearchBox showMore limitMax={52} transformItems={items => orderBy(items, ['label', 'count'], ['asc', 'desc'])} />
         </Panel>
       </Col>
-      <Col sm={7}>
-        <div>
-          <SearchBox autoFocus />
-        </div>
-        <div className={'Catalog__currentRefinements'}>
-          <CurrentRefinements />
-          <ClearAll />
-          <Stats />
-        </div>
+      <Col sm={10}>
         <Hits hitComponent={Record.bind(this, clearSearch)} />
         <div className={'Catalog__pagination'}>
           <Pagination />
