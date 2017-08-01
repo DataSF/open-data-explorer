1afcf603862bb94cb3b591933ef34394b976d3b0
Jason Lally <jason.lally@gmail.com>
2017-08-01 08:38:29 -0700

    Add utm metadata to sharing link for tracking

--- src/containers/ModalShare.js
+++ src/containers/ModalShare.js
@@ -9,7 +9,7 @@ const mapStateToProps = (state, ownProps) => {
   let queryState = Object.assign({}, state.query)
   delete queryState.isFetching
   const encodedJSON = encodeURIComponent(JSON.stringify(queryState))
-  const shareLink = BASE_HREF + ownProps.location.pathname + '?q=' + encodedJSON
+  const shareLink = BASE_HREF + ownProps.location.pathname + '?q=' + encodedJSON + '&utm_source=explorer&utm_medium=share_modal'
   return {
     showModal: state.ui.modals.share,
     shareLink
