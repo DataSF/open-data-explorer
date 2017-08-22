25fe9d40650228a66075d57718aabab70cd6aa6e
Jason Lally <jason.lally@gmail.com>
2017-08-22 14:16:57 -0700

    Pad maxPowerOf10 to accomodate larger domainRanges

--- src/helpers/findCeiling.js
+++ src/helpers/findCeiling.js
@@ -1,13 +1,13 @@
 
 module.exports = function findCeiling (maxValue, maxPowerOf10) {
-  let zerosRange = Array.apply(null, Array(maxPowerOf10)).map(function (_, i) { return i })
+  let zerosRange = Array.apply(null, Array(maxPowerOf10 + 2)).map(function (_, i) { return i })
   for (let i = 1; i < zerosRange.length; i++) {
-  let zeros = Math.pow(10, zerosRange[i])
-  let zerosNext = Math.pow(10, zerosRange[i + 1])
-  if ((zeros < maxValue) && (zerosNext > maxValue)) {
-    if (i !== 0) {
-      return Math.pow(10, zerosRange[i -1])
-    } else {
+    let zeros = Math.pow(10, zerosRange[i])
+    let zerosNext = Math.pow(10, zerosRange[i + 1])
+    if ((zeros < maxValue) && (zerosNext > maxValue)) {
+      if (i !== 0) {
+        return Math.pow(10, zerosRange[i -1])
+      } else {
         return zeros
       }
     }
